0a79f602774b4329f6f14b085f3c1080be42423a
SeongBong <chopisal@gmail.com>
2022-04-09 01:15:42 +0900

    useMutation, withHandler

---
+++ pages/api/users/enter.ts
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import withHandler from "@libs/server/withHandler";
+
+type Data = {
+  name: string;
+};
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log(req.body);
+  return res.status(200).end();
+}
+
+export default withHandler("POST", handler);
