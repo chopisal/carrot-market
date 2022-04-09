2c616f25e7321b1e6aff11f36bb94cc44a6ecf72
SeongBong <chopisal@gmail.com>
2022-04-09 22:34:37 +0900

    enter

--- pages/api/users/enter.ts
+++ pages/api/users/enter.ts
@@ -2,12 +2,20 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import withHandler from "@libs/server/withHandler";
 
-type Data = {
-  name: string;
-};
-
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client?.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
   return res.status(200).end();
 }
 
