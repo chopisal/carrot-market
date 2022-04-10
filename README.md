e82397d91a26eb6e737302cf6dfcfa3b86cfd313
SeongBong <chopisal@gmail.com>
2022-04-11 01:37:47 +0900

    authentication

--- README.md
+++ README.md
@@ -2,3 +2,25 @@
 
 react-hook-form
 https://react-hook-form.com/
+
+db update
+npx prisma db push
+
+npx prisma studio
+
+# iron session : https://github.com/vvo/iron-session
+
+- 서명, 암호화된 쿠키를 사용하는 nodejs stateless 세션 도구
+- JWT는 암호화되지 않고 서명이 되어있음
+- 유저가 안에 있는 정보를 볼 수 없음
+- 세션을 위한 백엔드 구축이 필요 없음
+
+세션 vs 토큰 vs 쿠키? 기초개념 잡아드림. 10분 순삭! (노마드코더 유튜브)
+https://www.youtube.com/watch?v=tosLBcAX1vk
+
+Iron session
+데이터를 저장하기 위해 서명되고 암호화된 쿠키를 사용하는 Node.js stateless session 유틸리티.
+Next.js, Express, NestJs, Fastify 및 모든 Node.js HTTP 프레임워크와 함께 작동합니다.
+
+32자 랜덤 비밀번호 Length 32로 설정 후 복사해서 사용
+https://1password.com/password-generator/
--- pages/api/users/enter.ts
+++ pages/api/users/enter.ts
@@ -1,22 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import withHandler from "@libs/server/withHandler";
+import client from "@libs/server/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
-  const payload = phone ? { phone: +phone } : { email };
-  const user = await client?.user.upsert({
-    where: {
-      ...payload,
+  const user = phone ? { phone } : { email };
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
+        },
+      },
     },
-    create: {
-      name: "Anonymous",
-      ...payload,
-    },
-    update: {},
   });
-  console.log(user);
-  return res.status(200).end();
+  console.log(token);
+  return res.json({
+    ok: true,
+  });
 }
 
 export default withHandler("POST", handler);
